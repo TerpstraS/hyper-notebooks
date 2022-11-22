9cb945c05a7bf09d18e0e33c634bce435fbe93bd
TerpstraS <sjoerd.terpstra@outlook.com>
2022-11-22 09:24:17 +0100

    batch job

---
+++ analysis_edge_submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#
+#SBATCH -J PYTEST
+#SBATCH -p short
+#SBATCH -t 0:15:00
+#SBATCH -n 1
+#SBATCH -o log_pytest.%j.o
+#SBATCH -e log_pytest.%j.e
+
+conda activate cmip6-hypercc
+srun python3 analysis_cmip6.py
