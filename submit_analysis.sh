76039f7e5d231ecd8856117b82e75c56ebd36a28
TerpstraS <sjoerd.terpstra@outlook.com>
2022-11-23 14:27:13 +0100

    removed rm log*

--- submit_analysis.sh
+++ submit_analysis.sh
@@ -2,12 +2,10 @@
 #
 #SBATCH -J analysis_cmip6
 #SBATCH -p short
-#SBATCH -t 0:05:00
+#SBATCH -t 0:10:00
 #SBATCH -n 1
 #SBATCH -o log_pytest.%j.o
 #SBATCH -e log_pytest.%j.e
-
-rm log_pytest*
 
 conda activate cmip6-hypercc
 srun python3 analysis_cmip6.py
