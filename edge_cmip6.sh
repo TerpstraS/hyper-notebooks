c4decb3443a8838dd4f980d48d6fc9057dea5a81
Sjoerd Terpstra <sjoerd.terpstra@outlook.com>
2022-11-25 10:26:04 +0100

    hyperc script

---
+++ edge_cmip6.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+#
+#SBATCH -J edge_cmip6
+#SBATCH -p short
+#SBATCH -t 0:10:00
+#SBATCH -n 1
+#SBATCH -o log_edge_cmip6.%j.o
+#SBATCH -e log_edge_cmip6.%j.e
+
+conda activate cmip6-hypercc
+
+hyperccpath = "/nethome/terps020/edge/hypercc/bin"
+outpath = "/nethome/terps020/edge/output"
+datapath = "/nethome/terps020/cmip6/data"
+option_single = "--single"
+option_month = "--annual"
+
+sigmaS = "100"
+sigmaT = "10"
+thresh1 = pi-control-max
+thresh2 = pi-control-max*1/2
+
+model = "IPSL-CM6A-LR"
+scen = "1pctCO2"
+var = "tas"
+rea = "r1i1p1f1"
+
+### clear cache - otherwise it fills the whole hard drive...
+rm -f hypercc-cache.hdf5 cache.lock hypercc-cache.db
+
+${hyperccpath}/hypercc ${option_single} --data-folder ${datapath} --pi-control-folder ${datapath}  \
+  report --variable ${var} --model ${model} --scenario ${scen} --realization ${rea} \
+  ${option_month} --sigma-t ${sigmaT} year --sigma-x ${sigmaS} km \
+  --upper-threshold ${thresh1} --lower-threshold ${thresh2}
