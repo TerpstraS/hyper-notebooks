16fbfdedd1b52a8712a901750b9d09abdc8e7ace
Sjoerd Terpstra <sjoerd.terpstra@outlook.com>
2022-11-28 16:24:51 +0100

    data format lan lot to x y

--- format_data.py
+++ format_data.py
@@ -4,7 +4,7 @@ import xarray as xr
 
 DIR_DATA = os.path.join("/nethome", "terps020", "cmip6", "data")
 
-fname = os.path.join(DIR_DATA, "CMIP.GFDL-ESM4.1pctCO2.r1i1p1f1.SImon.siconc.gr.nc")
+fname = os.path.join(DIR_DATA, "pre_CMIP.GFDL-ESM4.1pctCO2.r1i1p1f1.SImon.siconc.gr.nc")
 ds_disk = xr.open_dataset(fname)
 ds_disk["lat"] = ds_disk.y
 ds_disk["lon"] = ds_disk.x
@@ -13,7 +13,7 @@ ds_disk.to_netcdf(os.path.join(DIR_DATA, fname))
 del ds_disk
 
 
-fname = os.path.join(DIR_DATA, "CMIP.GFDL-ESM4.piControl.r1i1p1f1.SImon.siconc.gr.nc")
+fname = os.path.join(DIR_DATA, "pre_CMIP.GFDL-ESM4.piControl.r1i1p1f1.SImon.siconc.gr.nc")
 ds_disk = xr.open_dataset(fname)
 ds_disk["lat"] = ds_disk.y
 ds_disk["lon"] = ds_disk.x
