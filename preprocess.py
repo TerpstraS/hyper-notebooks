f22267c315a677ab71fac44c2aac3341176adc75
Sjoerd Terpstra <sjoerd.terpstra@outlook.com>
2022-11-30 14:45:51 +0100

    new download and preprocess structure

--- preprocess.py
+++ preprocess.py
@@ -83,89 +83,39 @@ def find_filename(dir, experiment_id):
 
 if __name__ == '__main__':
 
-    # get essential info from bash script as input arguments
-    OPENID = sys.argv[1]
-    PASSWORD = sys.argv[2]
-
-    # connection is needed to be able to execute the wget scripts
-    # lm = login(OPENID, PASSWORD)
-
-    experiment_id = sys.argv[3]
-    variable = sys.argv[4]
-    wget_var = sys.argv[5]  # this is including the whole path
-    wget_var = wget_var.split("/")[-1] # we want only file name, not path
+    experiment_id = sys.argv[1]
+    variable = sys.argv[2]
 
     # directory where to save the downloaded files
-    DIR_DATATEMP_WGET = os.path.join("/nethome", "terps020", "cmip6", "datatemp", "wget")
-    if not os.path.isdir(DIR_DATATEMP_WGET):
-        os.makedirs(DIR_DATATEMP_WGET)
-    DIR_WGET_SCEN = os.path.join(
-        "/nethome", "terps020", "cmip6", "wget", variable, experiment_id
-    )
-    DIR_WGET_PICONTROL = os.path.join(
-        "/nethome", "terps020", "cmip6", "wget", variable, "piControl"
-    )
+    DIR_DATATEMP = os.path.join("/nethome", "terps020", "cmip6", "datatemp")
+    if not os.path.isdir(DIR_DATATEMP):
+        os.makedirs(DIR_DATATEMP)
 
-    # check if piControl file exists
-    wget_piControl = wget_var.split(".").copy()
-    wget_piControl[2] = "piControl"
-    wget_piControl = ".".join(wget_piControl)
-    print(wget_var, wget_piControl)
-    if not os.path.isfile(os.path.join(DIR_WGET_PICONTROL, wget_piControl)):
-        raise RuntimeError("No associated piControl wget script to {}".format(wget_var))
+    # open files and preprocess them
+    #TODO: ds_var_fname --> how to obtain this?
+    ds_var_fname = find_filename(DIR_DATATEMP, experiment_id)
+    ds_var_path = os.path.join(DIR_DATATEMP, ds_var_fname)
+    ds_var = xr.open_dataset(ds_var_path)
+    ds_var = preprocessing_wrapper(ds_var)
 
-    ## WARNING: temporary only use files that are already in gr format
-    ## TODO: remapping files (either here or by using cdo in bash)
-    print("# WARNING: temporary only using files that are already in gr format")
-    if not wget_var.endswith("gr.sh"):
-        raise RuntimeError(
-            "# WARNING: temporary only using files that are already in gr format"
-        )
+    ## TODO: Concatenate files if necessary
 
-    # make sure files are executable
-    wget_var_path = os.path.join(DIR_WGET_SCEN, wget_var)
-    wget_piControl_path = os.path.join(DIR_WGET_PICONTROL, wget_piControl)
-    os.chmod(wget_var_path, 0o750)
-    os.chmod(wget_piControl_path, 0o750)
+    # save and remove from memory to speed-up and save space
+    ## TODO: make sure to save to correct file name (should be correct now)
+    ds_var_fname_save = ".".join(wget_var.split(".")[:-1]) + ".nc"
+    ds_var.to_netcdf(os.path.join(DIR_DATATEMP, ds_var_fname_save))
+    del ds_var
 
-    # copy wget script to datatemp directory so we can easily use them in the bash script
-    shutil.copy(wget_var_path, os.path.join(DIR_DATATEMP_WGET, wget_var))
-    shutil.copy(wget_piControl_path, os.path.join(DIR_DATATEMP_WGET, wget_piControl))
+    ds_piControl_fname = find_filename(DIR_DATATEMP, "piControl")
+    ds_piControl_path = os.path.join(DIR_DATATEMP, ds_piControl_fname)
+    ds_piControl = xr.open_dataset(ds_piControl_path)
+    ds_piControl = preprocessing_wrapper(ds_piControl)
 
+    ## TODO: Concatenate files if necessary
 
-    # # subprocess.check_output("bash {} -s".format(wget_var_path), cwd=DIR_DATATEMP)
-    # # subprocess.check_output("bash {} -s".format(wget_piControl_path), cwd=DIR_DATATEMP)
-    # process = subprocess.call(["bash", wget_var_path, "-s", "-d"], cwd=DIR_DATATEMP)
-    # # process.wait()
-    # process = subprocess.call(["bash", wget_piControl_path, "-s", "-d"], cwd=DIR_DATATEMP)
-    # # process.wait()
-    # # can speed up above by waiting only once instead of twice
-    #
-    # # open files and preprocess them
-    # #TODO: ds_var_fname --> how to obtain this?
-    # ds_var_fname = find_filename(DIR_DATATEMP, experiment_id)
-    # ds_var_path = os.path.join(DIR_DATATEMP, ds_var_fname)
-    # ds_var = xr.open_dataset(ds_var_path)
-    # ds_var = preprocessing_wrapper(ds_var)
-    #
-    # ## TODO: Concatenate files if necessary
-    #
-    # # save and remove from memory to speed-up and save space
-    # ## TODO: make sure to save to correct file name (should be correct now)
-    # ds_var_fname_save = ".".join(wget_var.split(".")[:-1]) + ".nc"
-    # ds_var.to_netcdf(os.path.join(DIR_DATATEMP, ds_var_fname_save))
-    # del ds_var
-    #
-    # ds_piControl_fname = find_filename(DIR_DATATEMP, "piControl")
-    # ds_piControl_path = os.path.join(DIR_DATATEMP, ds_piControl_fname)
-    # ds_piControl = xr.open_dataset(ds_piControl_path)
-    # ds_piControl = preprocessing_wrapper(ds_piControl)
-    #
-    # ## TODO: Concatenate files if necessary
-    #
-    # # save and remove from memory to speed-up and save space
-    # ## TODO: make sure to save to correct file name (should be correct now)
-    # # ds_piControl_fname = "CMIP.source_id.experiment_id.member_id.table_id.variable_id.gr.nc"
-    # ds_piControl_fname_save = ".".join(wget_piControl.split(".")[:-1]) + ".nc"
-    # ds_piControl.to_netcdf(os.path.join(DIR_DATATEMP, ds_piControl_fname_save))
-    # del ds_piControl
+    # save and remove from memory to speed-up and save space
+    ## TODO: make sure to save to correct file name (should be correct now)
+    # ds_piControl_fname = "CMIP.source_id.experiment_id.member_id.table_id.variable_id.gr.nc"
+    ds_piControl_fname_save = ".".join(wget_piControl.split(".")[:-1]) + ".nc"
+    ds_piControl.to_netcdf(os.path.join(DIR_DATATEMP, ds_piControl_fname_save))
+    del ds_piControl
